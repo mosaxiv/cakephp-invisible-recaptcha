94cb6b33ad91897641691147eb8d7ec2b36b7ef8
mosaxiv <mosaxiv@gmail.com>
2017-12-26 00:31:38 +0900

    add InvisibleReCaptchaComponent

---
+++ src/Controller/Component/InvisibleReCaptchaComponent.php
@@ -0,0 +1,12 @@
+<?php
+namespace InvisibleReCaptcha\Controller\Component;
+
+use Cake\Controller\Component;
+
+/**
+ * InvisibleReCaptcha component
+ */
+class InvisibleReCaptchaComponent extends Component
+{
+
+}
