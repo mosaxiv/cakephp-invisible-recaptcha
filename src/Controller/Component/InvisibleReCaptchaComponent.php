f5b488c5c0f5f5a4765d0af04130d3509615e97a
mosaxiv <mosaxiv@gmail.com>
2017-12-28 02:31:25 +0900

    add verify method to InvisibleReCaptchaComponent

--- src/Controller/Component/InvisibleReCaptchaComponent.php
+++ src/Controller/Component/InvisibleReCaptchaComponent.php
@@ -2,11 +2,51 @@
 namespace InvisibleReCaptcha\Controller\Component;
 
 use Cake\Controller\Component;
+use Cake\Http\Client;
 
 /**
  * InvisibleReCaptcha component
  */
 class InvisibleReCaptchaComponent extends Component
 {
+    const VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify';
+
+    protected $_defaultConfig = [
+        'secretkey' => '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize(array $config)
+    {
+    }
+
+    /**
+     * @return bool
+     */
+    public function verify()
+    {
+        $response = $this->_sendRequest();
+
+        return isset($response['success']) && $response['success'] === true;
+    }
+
+    /**
+     * @return array
+     */
+    protected function _sendRequest()
+    {
+        $request = $this->getController()->request;
+
+        $http = new Client();
+        $response = $http->post(self::VERIFY_URL, [
+            'secret' => $this->getConfig('secretkey'),
+            'response' => $request->getData('g-recaptcha-response'),
+            'remoteip' => $request->clientIp(),
+        ]);
+
+        return $response->json;
+    }
 
 }
